ef089dff918e200e748306cf4b3b6a1b0d9f06f3
Tian Yutong <tyt0306@connect.hku.hk>
2024-06-03 16:49:10 +0800

    145

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|Kristy|我|爱|你|#heart|');
+      S.UI.simulate('|Kristy|我|爱|你|');
     }
 
     S.Drawing.loop(function () {
